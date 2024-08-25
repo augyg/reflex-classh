c808665571cc5d6524b26337ed29451fc6bf0756
lazyLambda <galen.sprout@gmail.com>
2024-08-25 19:48:37 -0400

    add reflex as nix dep

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, base, data-default, lens, lib, template-haskell
-, text
+, text, reflex-dom-core
 }:
 mkDerivation {
   pname = "reflex-classhss";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base data-default lens template-haskell text
+    base data-default lens template-haskell text reflex-dom-core
   ];
   homepage = "https://github.com/augyg/ClasshSS";
   description = "Typified Tailwind for Rapid Development";
