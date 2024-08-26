33ccd3a898198cda6807702c6e05d251b81b4a74
lazyLambda <galen.sprout@gmail.com>
2024-08-25 20:48:21 -0400

    na

--- default.nix
+++ default.nix
@@ -1,17 +1,14 @@
 { mkDerivation, base, data-default, lens, lib, template-haskell
 , text
 , reflex-dom-core
-#, ClasshSS
 , pkgs
+, ClasshSS ? pkgs.haskellPackages.callPackage (pkgs.fetchFromGitHub {
+  owner = "augyg";
+  repo = "ClasshSS";
+  rev = "bb41d2a6a756180c2aed3db567c69653a495d92e";
+  sha256 = "sha256-/PB6iQqcYJWzpyEcQlI+h0DzQfg39sCwo/QzQupcEvQ=";
+}) {}
 }:
-let
-  ClasshSS = pkgs.haskellPackages.callPackage (pkgs.fetchFromGitHub {
-    owner = "augyg";
-    repo = "ClasshSS";
-    rev = "bb41d2a6a756180c2aed3db567c69653a495d92e";
-    sha256 = "sha256-/PB6iQqcYJWzpyEcQlI+h0DzQfg39sCwo/QzQupcEvQ=";
-  }) {};
-in
 mkDerivation {
   pname = "reflex-classhss";
   version = "0.1.0.0";
